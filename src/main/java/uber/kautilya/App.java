d5bc986da59f0e6e768ff024bbb60be6d0eb1402
Mithun <mithuncm@gmail.com>
2023-01-09 17:35:42 +0530

    Thread.sleep() to put the thread on pause for a set duration. Also, use case for a daemon thread

--- src/main/java/uber/kautilya/App.java
+++ src/main/java/uber/kautilya/App.java
@@ -2,11 +2,27 @@ package uber.kautilya;
 
 import uber.kautilya.prime.PrimeNumberUtil;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
   public static void main(String[] args) {
+
+    List<Thread> threadList = new ArrayList<>();
+    Thread statusThread = new Thread(() -> {
+      try {
+        //Pause for 5 seconds - static method sleep() on current thread - Timed Waiting Lifecycle state
+        Thread.sleep(5000);
+        printThreadStatus(threadList);
+      } catch (InterruptedException e) {
+        System.out.println("Exception: " + e.getMessage());
+      }
+    }, "statusThread");
+    //Daemon: This thread ends when the main thread is ended
+    statusThread.setDaemon(true);
+    statusThread.start();
 
     //The while loop is not brocked as each calculation is done in a separate thread
     while (true) {
@@ -26,9 +42,20 @@ public class App {
       //When the main method thread ends, daemon threads are killed
       //Useful in monitoring, logging functions etc., which have meaning while the main application is alive
       t.setDaemon(true);
+      threadList.add(t);
       t.start();
     }
 
+  }
+
+  private static void printThreadStatus(List<Thread> threadList) throws InterruptedException {
+    while (true) {
+      System.out.println("\nThread Status: ");
+      threadList.forEach(t -> {
+        System.out.println("Name: " + t.getName());
+        System.out.println("Status: " + t.getState());
+      });
+    }
   }
 
 }
