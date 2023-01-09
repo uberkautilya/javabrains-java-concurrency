6b873a0ad03e3faa8fb35fb00dd5bbaf9f29057d
Mithun <mithuncm@gmail.com>
2023-01-09 13:18:12 +0530

    git init

---
+++ src/main/java/uber/kautilya/App.java
@@ -0,0 +1,9 @@
+package uber.kautilya;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ src/test/java/uber/kautilya/AppTest.java
@@ -0,0 +1,12 @@
+package uber.kautilya;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void testMain() {
+    App.main(null);
+  }
+
+}
